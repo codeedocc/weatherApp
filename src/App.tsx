c271ec996eca43d0d1aa6462b3e4000b2be568d7
Ruslan Sharifullin <newwavecfy@gmail.com>
2022-12-25 23:58:51 +0300

    created Location component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import axios from 'axios'
+import Location from './components/Location'
 
 function App() {
-  return <div className="App">Hello</div>
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Kazan&appid=2b573449087270184ae0fb5758061b01`
+
+  return (
+    <div className="app">
+      <div className="container">
+        <Location />
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Location.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Location() {
+  return (
+    <div className="top">
+      <div className="location">
+        <p>Казань</p>
+      </div>
+      <div className="temp">-1°C</div>
+      <div className="description">
+        <p>Облачно</p>
+      </div>
+    </div>
+  )
+}
+
+export default Location
