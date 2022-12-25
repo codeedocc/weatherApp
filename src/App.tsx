d4acf28899b0df0eaf8e89bc383242842dffcac8
Ruslan Sharifullin <newwavecfy@gmail.com>
2022-12-26 00:02:19 +0300

    created Indicators component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import axios from 'axios'
 import Location from './components/Location'
+import Indicators from './components/Indicators'
 
 function App() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=Kazan&appid=2b573449087270184ae0fb5758061b01`
@@ -9,6 +10,7 @@ function App() {
     <div className="app">
       <div className="container">
         <Location />
+        <Indicators />
       </div>
     </div>
   )
---
+++ src/components/Indicators.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Indicators() {
+  return (
+    <div className="bottom">
+      <div className="feels">
+        <p>-4°C</p>
+      </div>
+      <div className="humidity">
+        <p>87%</p>
+      </div>
+      <div className="wind">
+        <p>3 м/с</p>
+      </div>
+    </div>
+  )
+}
+
+export default Indicators
