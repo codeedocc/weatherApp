d42e371fae18ceb4ac3de375d7b21cd24da98eec
Ruslan Sharifullin <newwavecfy@gmail.com>
2022-12-26 03:21:19 +0300

    showing dynamic data from api

--- src/components/Indicators.jsx
+++ src/components/Indicators.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { useAppSelector } from '../hooks/redux'
 
 function Indicators() {
+  const { data } = useAppSelector((state) => state.weather)
+
   return (
     <div className="bottom">
       <div className="feels">
-        <p className="bold">-4°C</p>
+        {data.main ? <p className="bold">{data.main.feels_like}°C</p> : null}
         <p>Ощущается как</p>
       </div>
       <div className="humidity">
-        <p className="bold">87%</p>
+        {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
         <p>Влажность</p>
       </div>
       <div className="wind">
-        <p className="bold">3 м/с</p>
+        {data.wind ? <p className="bold">{data.wind.speed} м/с</p> : null}
         <p>Скорость ветра</p>
       </div>
     </div>
--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -10,10 +10,10 @@ function Location() {
         <p>{data.name}</p>
       </div>
       <div className="temp">
-        {data.main ? <h1>{data.main.temp}°</h1> : null}
+        {data.main ? <h1>{data.main.temp}°C</h1> : null}
       </div>
       <div className="description">
-        <p>Облачно</p>
+        {data.weather ? <p>{data.weather[0].description}</p> : null}
       </div>
     </div>
   )
