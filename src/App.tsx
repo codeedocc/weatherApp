d1aceafa7bfc8268075eb4126d40da02a91d377d
Ruslan Sharifullin <newwavecfy@gmail.com>
2023-01-27 19:24:16 +0300

    change background accoring to temperature

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,27 @@ import React from 'react'
 import Location from './components/Location'
 import Indicators from './components/Indicators'
 import Input from './components/Input'
+import { useAppSelector } from './hooks/redux'
+import { useDebounce } from './hooks/debounce'
+import { useSearchCitiesQuery } from './store/weather/weather.api'
 
 function App() {
+  const { location } = useAppSelector((state) => state.weather)
+  const debounced = useDebounce(location)
+
+  const { data } = useSearchCitiesQuery(debounced, {
+    skip: debounced.length < 3,
+    refetchOnFocus: true,
+  })
+
   return (
-    <div className="wrapped">
+    <div
+      className={`wrapped ${
+        data?.name !== undefined && data?.main.feels_like > 0 && 'warm'
+      } ${data?.name !== undefined && data?.main.feels_like < 0 && 'cold'} ${
+        data?.name === undefined && 'default'
+      }`}
+    >
       <Input />
       <Location />
       <Indicators />
